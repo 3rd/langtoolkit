43dcf60f08e803f991b50b38adb6627524c4562c
3rd <3rd@users.noreply.github.com>
2023-07-04 14:31:07 +0300

    fix: Card - support cards without header

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -18,7 +18,7 @@ Card.displayName = "Card";
 
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />
+    <div ref={ref} className={cn("flex flex-col space-y-1.5 px-6 pt-6 mb-6", className)} {...props} />
   )
 );
 CardHeader.displayName = "CardHeader";
@@ -38,7 +38,7 @@ const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttribu
 CardDescription.displayName = "CardDescription";
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  ({ className, ...props }, ref) => <div ref={ref} className={cn("p-6 pt-0 mt-6", className)} {...props} />
 );
 CardContent.displayName = "CardContent";
 
