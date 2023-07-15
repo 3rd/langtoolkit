7fff264d6dba4d3ad6857415e303727ec01e9b52
3rd <3rd@users.noreply.github.com>
2023-07-16 01:29:24 +0300

    chore: setup path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }],
+  },
   server: {
     host: "localhost",
     open: true,
